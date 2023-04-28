55c43522f9d75ade699f2bd6e31107b7ad6bb8c7
Duku Aghogho Mathida <mathildaduku@gmail.com>
2023-04-28 22:56:56 +0000

    post confirmation lamda

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -1,5 +1,6 @@
 import json
 import psycopg2
+import os
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
@@ -11,32 +12,33 @@ def lambda_handler(event, context):
     user_handle        = user['preferred_username']
     user_cognito_id    = user['sub']    
     try:
-        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-        cur = conn.cursor()
 
-        sql = f"""
-          "INSERT INTO users (
-            display_name,
-            email, 
-            handle, 
-            cognito_user_id
-            ) 
-          VALUES(
-            {user_display_name},
-            {user_email},
-            {user_handle},
-            {user_cognito_id}
-          )"
-        """  
-        cur.execute(sql)
-        conn.commit() 
+      sql = f"""
+        INSERT INTO public.users (
+          display_name,
+          email, 
+          handle, 
+          cognito_user_id
+          ) 
+        VALUES(
+          '{user_display_name}',
+          '{user_email}',
+          '{user_handle}',
+          '{user_cognito_id}'
+        )  
+      print('SQL Statement ----')
+      print(sql)
+      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+      cur = conn.cursor()
+      cur.execute(sql)
+      conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+      print(error)
     finally:
-        if conn is not None:
-            cur.close()
-            conn.close()
-            print('Database connection closed.')
+      if conn is not None:
+          cur.close()
+          conn.close()
+          print('Database connection closed.')
 
     return event
