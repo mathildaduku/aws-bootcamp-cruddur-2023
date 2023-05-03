4ee9bd7668c7a1f902bdefb2ddac30bcda86353d
Duku Aghogho Mathida <mathildaduku@gmail.com>
2023-05-03 21:36:32 +0000

    fix errors

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -17,7 +17,7 @@ class Db:
     green = '\033[92m'
     no_color = '\033[0m'
     print("\n")
-    print(f'(green) Load SQL Template: (template_path) (no_color)')
+    print(f'{green} Load SQL Template: {template_path} {no_color}')
 
     with open(template_path, 'r') as f:
       template_content = f.read()
@@ -31,16 +31,16 @@ class Db:
   def print_params(self,params):
     blue = '\033[94m'
     no_color = '\033[0m'
-    print(f'(blue) SQL Params:(no_color)')
+    print(f'{blue} SQL Params:{no_color}')
     for key, value in params.items():
       print(key, ":", value)
 
   def print_sql(self,title,sql):
     cyan = '\033[96m'
     no_color = '\033[0m'
-    print(f'(cyan) SQL STATEMENT-[(title)]-----------(no_color)')
+    print(f'{cyan} SQL STATEMENT-[{title}]-----------{no_color}')
     print(sql)    
-  def query_commit(self,sql,params:{}):
+  def query_commit(self,sql,params={}):
     self.print_sql('commit with returning',sql)
 
     pattern = r"\bRETURNING\b"
@@ -58,7 +58,7 @@ class Db:
     except Exception as err:
       self.print_sql_err(err)
 # when we want to return a json object 
-  def query_array_json(self,sql,params:{}):
+  def query_array_json(self,sql,params={}):
     self.print_sql('array',sql)
 
     wrapped_sql = self.query_wrap_array(sql)
@@ -68,7 +68,7 @@ class Db:
         json  = cur.fetchone()
         return json[0] 
 # when we want to return an array of json objects
-  def query_object_json(self,sql,params:{}):
+  def query_object_json(self,sql,params={}):
 
     self.print_sql('json',sql)
     self.print_params(params)
--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta, timezone
 
 from lib.db import db
+
 class CreateActivity:
   def run(message, user_handle, ttl):
     model = {
