d40393f207ae5da8df0f8614ae9f39c0b4066a34
Duku Aghogho Mathida <mathildaduku@gmail.com>
2023-05-04 10:05:57 +0000

    bug fixes

--- backend-flask/db/sql/activities/create.sql
+++ backend-flask/db/sql/activities/create.sql
@@ -1,14 +1,14 @@
-INSERT INTO public.actiities (
-    user_uuid,
-    message,
-    expires_at
+INSERT INTO public.activities (
+  user_uuid,
+  message,
+  expires_at
 )
 VALUES (
-    (SELECT uuid 
+  (SELECT uuid 
     FROM public.users 
     WHERE users.handle = %(handle)s
     LIMIT 1
-    ),
-    %(message)s,
-    %s(expires_at)s
-    ) RETURNING uuid;
+  ),
+  %(message)s,
+  %(expires_at)s
+) RETURNING uuid;
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,15 +3,15 @@ from opentelemetry import trace
 
 from lib.db import db
 
-tracer = trace.get_tracer("home.activities")
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
     #logger.info("HomeActivities")
-    # with tracer.start_as_current_span("home-activities-mock-data"):
-    #   span = trace.get_current_span()
-    #   now = datetime.now(timezone.utc).astimezone()
-    #   span.set_attribute("app.now", now.isoformat())      
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
     sql = db.template('activities','home')
     results = db.query_array_json(sql)
     return results
